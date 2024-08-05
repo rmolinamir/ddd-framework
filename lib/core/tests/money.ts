e8fec5e8dd2c3b4e0ce2b2353f5e698a60c8f832
Robert Molina <rmolinamir@gmail.com>
2024-08-05 00:32:53 -0400

    chore(case): fixed pascal case file

---
+++ lib/core/tests/money.ts
@@ -0,0 +1,36 @@
+import { InvalidOperationException, ValueObject } from '../src';
+import { CurrencyDetails } from './currency-details';
+
+export class Money extends ValueObject {
+  public amount: number;
+
+  public currency: CurrencyDetails;
+
+  constructor(amount: number, currency: CurrencyDetails) {
+    super();
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  public add(summand: Money): Money {
+    if (this.currency.notEquals(summand.currency))
+      throw new InvalidOperationException(
+        'Cannot sum amounts with different currencies'
+      );
+
+    return new Money(this.amount + summand.amount, this.currency);
+  }
+
+  public substract(subtrahend: Money): Money {
+    if (this.currency.notEquals(subtrahend.currency))
+      throw new InvalidOperationException(
+        'Cannot substract amounts with different currencies'
+      );
+
+    return new Money(this.amount - subtrahend.amount, this.currency);
+  }
+
+  public toString(): string {
+    return this.currency.serialize(this);
+  }
+}
