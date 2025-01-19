249819084f5bdae4991dc891cec607f6661970f1
Robert Molina <rmolinamir@gmail.com>
2025-01-19 02:40:37 -0500

    feat(postgres): exporting pools for cjs

--- lib/postgres/src/index.ts
+++ lib/postgres/src/index.ts
@@ -2,4 +2,5 @@ export * from './pg-transaction.js';
 export * from './pg-transaction-manager.js';
 export * from './nestjs/drizzle-orm.module.js';
 export * from './nestjs/pg-client.module.js';
+export * from './nestjs/pg-pool.module.js';
 export * from './nestjs/pg-transaction-manager.module.js';
